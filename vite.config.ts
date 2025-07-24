987becce5fda756a3466efda041f1aea0fb909e7
Dan Dyar <dan@parameterz.com>
2025-07-23 22:40:29 -0700

    base url fix for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/cardiac-scaling-dev/', // Add this line!
   resolve: {
     alias: {
-      '@': '/src',
+      '@': new URL('./src', import.meta.url).pathname,
     },
   },
   optimizeDeps: {
